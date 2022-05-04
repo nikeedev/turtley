911983247e4de5dc6afca5c7dfdbf0683cc884c0
nikeedev <myftr12394@gmail.com>
2022-05-04 12:31:12 +0200

    updates!

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 from turtle import *
 
 pensize(20)
@@ -8,16 +8,16 @@ hideturtle()
 while True:
     times = 0
     for _ in range(100):
-        left(89)
+        left(random.randint(46, 89))
         forward(80)
 
 
     if times == 5:
         times = 0
+        clear()
         penup()
         goto(0, 0)
         pendown()
-        clear()
 
 
     penup()
