bfa3bd31c8e1ba1dd7f1fca547497440a1743ef1
nikeedev <myftr12394@gmail.com>
2022-05-04 14:53:55 +0200

    updates!

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
 import random
 from turtle import *
 
-pensize(20)
-speed(120)
-hideturtle()
+vinkel = 16
 
-while True:
-    times = 0
-    for _ in range(100):
-        left(random.randint(46, 89))
-        forward(80)
+for _ in range(vinkel):
+    forward((vinkel-2))
+    right(360/vinkel)
 
-
-    if times == 5:
-        times = 0
-        clear()
-        penup()
-        goto(0, 0)
-        pendown()
-
-
-    penup()
-    forward(120)
-    pendown()
-    times += 1
-
-
+input()
